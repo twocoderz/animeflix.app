1bcd83255c46f29f6cbcdf110fae1b40bfbc2758
Rahim10020 <rahialighi@gmail.com>
2026-08-24 16:37:13 +0000

    fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import Logo from "./Logo";
 
 export default function Header() {
   return (
-    <header className="flex bg-background shrink-0 items-center justify-between mx-12 mt-12 rounded-4xl px-6 py-2 sm:px-10 sm:py-7 lg:px-8">
-      <Logo size={256} />
+    <header className="flex bg-background shrink-0 items-center justify-between mx-12 rounded-4xl px-6 py-8 lg:px-8">
+      <Logo size={128} />
       <img
         src="/icons/androidIcon.svg"
         alt="Android"
-        className="h-8 w-8 sm:h-10 sm:w-10"
+        className="h-8 w-8 sm:h-12 sm:w-12"
       />
     </header>
   );
