3b3da1c1dc513ca856dd5d2d31de899e2f15afd5
Rahim10020 <rahialighi@gmail.com>
2026-08-24 15:46:16 +0000

    change logos

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -135,53 +135,58 @@ export default function HomePage() {
         {/* Footer – logos TV */}
         <footer className="flex flex-wrap justify-center items-center gap-5 md:gap-7 mt-8 shrink-0">
           <img
-            src="/tv_logos/filmrise-anime.png"
-            alt="FilmRise Anime"
-            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
-          />
-          <img
-            src="/tv_logos/mr-bean.png"
-            alt="Mr Bean"
-            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
-          />
-          <img
-            src="/tv_logos/duck.png"
-            alt="Duck"
-            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
-          />
-          <img
-            src="/tv_logos/naruto.png"
-            alt="Naruto"
-            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
-          />
-          <img
-            src="/tv_logos/retro-crush.png"
-            alt="Retro Crush"
-            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
-          />
-          <img
-            src="/tv_logos/yu-gi-oh.png"
-            alt="Yu-Gi-Oh!"
-            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
-          />
-          <img
-            src="/tv_logos/tiny-pop.png"
-            alt="Tiny Pop"
-            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
-          />
-          <img
-            src="/tv_logos/daria.png"
+            src="/tv_logos/01_Daria.png"
             alt="Daria"
             className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
           />
           <img
-            src="/tv_logos/pluto-toons.png"
-            alt="Pluto Toons"
+            src="/tv_logos/02_Duck%20TV.png"
+            alt="Duck TV"
             className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
           />
           <img
-            src="/tv_logos/gong.png"
+            src="/tv_logos/03_FilmRise%20Anime.png"
+            alt="FilmRise Anime"
+            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
+          />
+          <img
+            src="/tv_logos/04_Gong.png"
             alt="Gong"
+            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
+          />
+          <img
+            src="/tv_logos/05_Mr%20Bean%20Anim%C3%A9.png"
+            alt="Mr Bean Animé"
+            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
+          />
+          <img
+            src="/tv_logos/06_Naruto.png"
+            alt="Naruto"
+            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
+          />
+          <img
+            src="/tv_logos/07_Pluto%20TV%20Retro%20Toons.png"
+            alt="Pluto TV Retro Toons"
+            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
+          />
+          <img
+            src="/tv_logos/08_Pluto%20TV%20Toons.png"
+            alt="Pluto TV Toons"
+            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
+          />
+          <img
+            src="/tv_logos/09_RetroCrush.png"
+            alt="RetroCrush"
+            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
+          />
+          <img
+            src="/tv_logos/10_Tiny%20Pop.png"
+            alt="Tiny Pop"
+            className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
+          />
+          <img
+            src="/tv_logos/11_Yu-Gi-Oh.png"
+            alt="Yu-Gi-Oh"
             className="h-8 md:h-10 object-contain opacity-90 hover:opacity-100 transition-opacity"
           />
         </footer>
